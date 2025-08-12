ba2482ae38c190e60c357fbfcb458b4e044f6b57
bobby-99 <soumyaranjan.bobby@gmail.com>
2025-08-12 22:41:04 +0530

    leetcode.py changes

--- Day12/leetcode.py
+++ Day12/leetcode.py
@@ -1,1 +1,29 @@
-print("Hello world")
+# 70. Climbing Stairs
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        one, two = 1, 1
+        for i in range(n - 1):
+            temp = one
+            one = one + two
+            two = temp
+        return one
+    
+
+# 2878. Ways to Express an Integer as Sum of Powers
+
+class Solution:
+    def numberOfWays(self, n, x):
+        mod = 10**9 + 7
+        dp = [0] * (n + 1)
+        dp[0] = 1
+
+        for i in range(1, n + 1):
+            val = i**x
+            if val <= n:
+                for i in range(n, val - 1, -1):
+                    dp[i] = (dp[i] + dp[i - val])
+            else:
+                break
+
+        return dp[n] % mod
+    
