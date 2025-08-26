ef654cf2053319028d0c780f8ca31b95bd7923aa
bobby-99 <soumyaranjan.bobby@gmail.com>
2025-08-26 22:58:36 +0530

    Day 26

---
+++ Day26/leetcode26.py
@@ -0,0 +1,10 @@
+# 3000. Maximum Area of Longest Diagonal Rectangle
+
+class Solution:
+    def areaOfMaxDiagonal(self, dimensions: list[list[int]]) -> int:
+        a, b = max(
+            dimensions,
+            key=lambda x: (x[0]**2 + x[1]**2, x[0] * x[1])
+        )
+        return a * b
+
